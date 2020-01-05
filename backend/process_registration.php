3d5f682fa8534af7974e3149e5873638ef5a4490
ranalli <andrea.ranalli@studenti.unimi.it>
2020-01-05 19:31:58 +0100

    working on fixing registration process

--- backend/process_registration.php
+++ backend/process_registration.php
@@ -15,10 +15,13 @@ if(isset($_POST['username'],$_POST['password'],$_POST['password2'],$_POST['level
     if($_POST['level'] != '2') $society = null;
     else $society = $_POST['society'];
 
+    echo "PASSWORDS:: ".$_POST['password']."<br>".$_POST['password2'];
+    die();
+
     $result = register_new_user($_POST['username'],$_POST['password'],$_POST['password2'],$_POST['level'],$society,$db);
 
     if($result === true){
-        //echo 'utente registrato con successo';
+        //utente registrato con successo
         header('Location: ../frontend/login.php?success=Utente registrato correttamente. E\' possibile eseguire il login');
     }
     else{
@@ -26,6 +29,6 @@ if(isset($_POST['username'],$_POST['password'],$_POST['password2'],$_POST['level
     }
 }
 else{
-    header('Location: ../frontend/register.php?error=Parametri non corretti '.$_POST['username']." ".$_POST['password']." ".$_POST['password2']." ".$_POST['level']);
+    header('Location: ../frontend/register.php?error=Parametri non corretti '.$_POST['username']." P1".$_POST['password']." P2".$_POST['password2']." ".$_POST['level']);
 }
 unset($_POST);
--- frontend/register.php
+++ frontend/register.php
@@ -25,15 +25,20 @@
     }
 
     function check_password() {
-        if(p2.value.localeCompare(p1.value) !== 0){
-            msg.className = "text-danger";
-            msg.innerHTML = "Le password non coincidono";
-            //p2.setCustomValidity("Le password non coincidono");
+        if(p1.value.localeCompare("") !== 0 && p2.value.localeCompare("") !== 0){
+            if(p2.value.localeCompare(p1.value) !== 0){
+                msg.className = "text-danger";
+                msg.innerHTML = "Le password non coincidono";
+                p2.setCustomValidity("Le password non coincidono");
+            }
+            else{
+                msg.className = "text-success";
+                msg.innerHTML = "Ok";
+                p2.setCustomValidity("");
+            }
         }
         else{
-            msg.className = "text-success";
-            msg.innerHTML = "Ok";
-            //p2.setCustomValidity("");
+            msg.innerHTML = "";
         }
     }
 
@@ -57,16 +62,17 @@
         echo '<div class="alert alert-success" role="alert">'.$_GET['success'].'</div><br><br>';
     }
 ?>
-<!--fixme: attributo required non sembra funzionare -->
+<!--fixme: password2 non viene cifrata con sha512 -->
     <form method="post" action="../backend/process_registration.php">
         <label class="col-form-label" for="u">Username</label><br>
         <input type="text" name="username" value="" id="u" maxlength="100" minlength="1" oninvalid="alert('Il nome utente è obbligatorio');" required/><br>
 
-        <label class="col-form-label" for="p1">Password</label><br>
-        <input type="password" name="password" value="" id="p1" oninput="check_password()" required/><br>
+        <label class="col-form-label" for="p1">Password (4 - 10 characters)</label><br>
+        <input type="password" name="pass" value="" id="p1" oninput="check_password()" minlength="4" maxlength="10" required/><br>
 
         <label class="col-form-label" for="p2">Repeat password</label><br>
-        <input type="password" name="password2" value="" oninvalid="alert('Le password devono essere uguali e non vuote');" id="p2" oninput="check_password()" required/> <p id="pmsg"></p> <br>
+        <input type="password" name="pass2" value="" onchange="" oninvalid="alert('Le password devono essere uguali e non vuote');" id="p2" oninput="check_password()" minlength="4" maxlength="10" required/>
+        <p id="pmsg"></p> <br>
 
         <label class="col-form-label" for="level">Level</label>
         <select name="level" class="custom-select-sm" id="level" onchange="toggle_society_select(this.value)" required>
@@ -87,7 +93,8 @@
                     <option value="<?php echo $row['id'];?>"><?php echo $row['long_name'];?></option>
                <?php }?>
         </select><br><br>
-        <input type="button" class="btn btn-primary" value="Register" onclick="formhash(this.form,this.form.p1);"/>
+        <input type="button" class="btn btn-primary" value="Register" onclick="formhash(this.form,this.form.p1,this.form.p2);"/>
+        <!--<input type="submit" class="btn btn-primary" value="Register" onclick="formhash(this.form,this.form.p1,this.form.p2);"/>-->
     </form>
 <button onclick="goBack()" class="btn btn-outline-secondary">< Back</button>
 
